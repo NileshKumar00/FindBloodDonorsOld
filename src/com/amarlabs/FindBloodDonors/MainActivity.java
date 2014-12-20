c0b529e56d03187aa035d5409e97d69796fe9f7e
Nilesh Kumar <nilkumar@paypal.com>
2014-12-20 14:34:16 +0530

    Launching Page

--- src/com/amarlabs/FindBloodDonors/MainActivity.java
+++ src/com/amarlabs/FindBloodDonors/MainActivity.java
@@ -1,21 +1,60 @@
 package com.amarlabs.FindBloodDonors;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.AdapterView.OnItemSelectedListener;
 
 
 public class MainActivity extends ActionBarActivity {
 
+	private Spinner spinner;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        setBloodGroupSpinnerContent();
+        
     }
 
+    private void setBloodGroupSpinnerContent() {
 
-    @Override
+    	final List<String> list = new ArrayList<String>();
+		list.add("A-");list.add("AB-");list.add("O-");list.add("B-");
+		list.add("A+");list.add("AB+");list.add("O+");list.add("B+");
+		
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, list);
+		
+		spinner = (Spinner) this.findViewById( R.id.spinner1);
+		spinner.setAdapter( adapter );
+		
+		spinner.setOnItemSelectedListener(new OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> arg0, View arg1, int position, long arg3) {
+                list.get(position);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> arg0) {
+
+            }
+        });
+    	
+	}
+
+
+	@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
